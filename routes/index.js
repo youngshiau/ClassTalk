678a5df428950b932b4bd5fefba949da9c53bad7
Young Shiau <youngshiau@gmail.com>
2015-02-10 13:49:14 -0800

    worked on views; still testing things out

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+/*
+var users = require('../users.jason');
+var threads = require('../threads.json');
+var comments = require('../comments.json');
+*/
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+	res.render('index', { title: 'Home', view: 'home' });
 });
 
 /* GET class page. */
+// passes in index.jade, but renders with different title
+router.get('/class', function(req, res, next) {
+	res.render('index', { title: 'CSE3', view: 'class' });
+});
 
 /* GET class thread */
 
